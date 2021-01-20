8389c3c18aa693894a6ed92c32b1d4beb3508894
Birger Topphol <birger.topphol@no.experis.com>
2021-01-20 17:23:04 +0100

    chore: set up bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 import router from "./router";
+import { BootstrapVue } from "bootstrap-vue";
 
+// Import Bootstrap (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 
 new Vue({
