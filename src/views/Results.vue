26f19db65c3b70a67a4a114f85152f09c9e89b6f
Birger Topphol <birger.topphol@no.experis.com>
2021-01-23 20:28:22 +0100

    feat: display score

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Results</h1>
+    <h2>Score: {{ score }}</h2>
 
     <Result
       v-for="question in questions"
@@ -21,6 +22,20 @@ export default {
     return {
       questions: this.$root.$data.questions
     };
+  },
+  computed: {
+    score() {
+      if (this.questions !== undefined) {
+        let score = 0;
+        for (const question of this.questions) {
+          if (question.given_answer === question.correct_answer) {
+            score += 10;
+          }
+        }
+        return score;
+      }
+      return null;
+    }
   }
 };
 </script>
